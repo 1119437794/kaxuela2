1bf916cffadf926b987e9fa1894abd2a8fafbabb
xiaochuanyang <xiaochuan.yang@56qq.com>
2018-05-24 13:37:17 +0800

    fix: 添加友情链接组件

---
+++ src/components/common/FriendLink/index.vue
@@ -0,0 +1,23 @@
+<style scoped>
+  @import './scoped.less';
+</style>
+<style>
+</style>
+<script>
+export { default } from './model'
+</script>
+<template>
+  <div class="root">
+    <h3 class="head">
+      <span class="title">友情链接</span>
+    </h3>
+    <div class="body">
+      <ul class="links">
+        <li class="link"><a href="">APP下载</a></li>
+        <li class="link"><a href="">拉勾云</a></li>
+        <li class="link"><a href="">中大网校</a></li>
+        <li class="link"><a href="">人人都是产品经历</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
