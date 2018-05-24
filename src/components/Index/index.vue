fb4876ad7641884e907c09f5caa958f343c527cc
limengjun <limengjun@bbdservice.com>
2018-05-24 19:03:46 +0800

    merge

--- src/components/Index/index.vue
+++ src/components/Index/index.vue
@@ -6,12 +6,8 @@ export { default } from './model'
 </script>
 <template>
   <div class="root">
-<<<<<<< HEAD
-    <Login />
-=======
     <Header />
     <FriendLink />
     <Footer />
->>>>>>> 8a2f0cae677df571ddeacde5c21976d9627384a3
   </div>
 </template>
--- src/components/Index/model.js
+++ src/components/Index/model.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { Header, Login } from '../common'
-
-export default {
-  name: 'Index',
-  components: {
-    Header,
-    Login
-=======
 export default {
   name: 'Index',
   components: {
     ...require('../common')
->>>>>>> 8a2f0cae677df571ddeacde5c21976d9627384a3
   },
   data () {
     return {
