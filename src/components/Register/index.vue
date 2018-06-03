7f1a82d107dc7aa0a4985ed018fdea5ba770c1fb
ren2432648aaa <654475466@qq.com>
2018-06-03 14:01:05 +0800

    完成注册页面

--- src/components/Register/index.vue
+++ src/components/Register/index.vue
@@ -1,11 +1,114 @@
 <style scoped lang="less">
   @import 'scoped.less';
 </style>
+<style lang="less">
+  @import './override.less';
+</style>
 <script>
 export { default } from './model'
 </script>
 <template>
   <div class="container">
+    <div class="header">
+      <a class="logo" href="">
+        <img
+          src="../../../static/imgs/logo.png"/>
+      </a>
+    </div>
+    <div class="banner" />
+    <div class="content">
+      <div class="steps">
+        <el-steps :active=registerType :space="120" align-center>
+          <el-step title="完善个人信息"></el-step>
+          <el-step title="手机验证"></el-step>
+        </el-steps>
+      </div>
+      <el-form
+        class="form"
+        v-show="registerType === 1"
+        :model="registerForm"
+        :rules="rules"
+        ref="registerForm"
+        label-width="100px"
+      >
+        <el-form-item label="头像" prop="headPortrait">
+          <el-upload
+            class="avatar-uploader"
+            :show-file-list="false"
+            :on-success="uploadOk"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-upload2 avatar-uploader-icon"></i>
+            <div slot="tip" class="el-upload__tip avater-upload-tip">
+              建议使用真实头像提升真实性、专业性
+              支持jpg、jpeg、gif、png,小于10MB
+            </div>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="用户名" prop="name">
+          <el-input
+            v-model="registerForm.name"
+            placeholder="用户名由英文和数字组成，长度不小于6位"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="昵称" prop="nickname">
+          <el-input
+            placeholder="请输入昵称"
+            v-model="registerForm.nickname"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            placeholder="请输入密码，有英文和数字组成,长度不小于6位"
+            type="password"
+            v-model="registerForm.password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="confirmPassword">
+          <el-input
+            placeholder="请输入确认密码"
+            type="password"
+            v-model="registerForm.confirmPassword"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="擅长" prop="goodAt">
+          <el-input
+            placeholder="请输入擅长内容"
+            v-model="registerForm.goodAt"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="next">
+          <div
+            @click="submitForm('registerForm')"
+            class="btn btn_green btn-radius"
+          >下一步</div>
+        </el-form-item>
+      </el-form>
+      <el-form
+        class="form"
+        v-show="registerType === 2"
+        :model="registerPhoneForm"
+        :rules="phoneRules"
+        ref="registerPhoneForm"
+        label-width="100px"
+      >
+        <el-form-item label="手机" prop="phone">
+          <el-input
+            placeholder="请输入常用手机号"
+            v-model="registerPhoneForm.phone"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="verificateCode">
+          <el-input
+            placeholder="请输入验证码"
+            v-model="registerPhoneForm.verificateCode"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="submit">
+          <div class="btn btn_green btn-radius">提交</div>
+        </el-form-item>
+      </el-form>
+    </div>
     <Footer />
   </div>
 </template>
--- src/components/Register/model.js
+++ src/components/Register/model.js
@@ -5,9 +5,67 @@ export default {
   name: 'Register',
   data () {
     return {
+      registerType: 1,
+      registerForm: {
+        imageUrl: '',
+        nickname: '',
+        name: '',
+        password: '',
+        confirmPassword: '',
+        goodAt: ''
+      },
+      registerPhoneForm: {
+        phone: '',
+        verificateCode: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { min: 6, message: '长度不小于6位', trigger: 'blur' }
+        ],
+        nickname: [
+          { required: true, message: '请输入昵称', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, message: '长度不小于6位', trigger: 'blur' }
+        ],
+        confirmPassword: [
+          { required: true, message: '请输入确认密码', trigger: 'blur' },
+          { min: 6, message: '长度不小于6位', trigger: 'blur' }
+        ],
+        goodAt: [
+          { required: true, message: '请输入擅长', trigger: 'blur' }
+        ]
+      },
+      phoneRules: {}
     }
   },
   methods: {
+    uploadOk (res, file) {
+      this.registerForm.imageUrl = URL.createObjectURL(file.raw)
+    },
+    beforeAvatarUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    },
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.registerType = 2
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
   },
   components: {
     Footer
