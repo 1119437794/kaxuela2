216740fc3210de1e6beeefdd98f2cbd4fe147ed7
xiaochuanyang <xiaochuan.yang@56qq.com>
2018-06-24 14:23:19 +0800

    fix: 大牛接口调试

--- src/components/common/DaNiuItem/index.vue
+++ src/components/common/DaNiuItem/index.vue
@@ -7,24 +7,24 @@ export { default } from './model'
 <template>
   <div class="daniu">
     <div class="one">
-      <img class="img" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="">
+      <img class="img" :src="dataSource.headimg" alt="">
       <div class="user">
         <span class="name">{{dataSource.name}}</span>
-        <span class="job">平面设计</span>
+        <span class="job">{{dataSource.good}}</span>
       </div>
     </div>
-    <p class="tag">wo wowo woow </p>
+    <p class="tag">{{dataSource.remark}}</p>
       <ul class="count">
         <li class="item">
-          <span class="num">50</span>
+          <span class="num">{{dataSource.video_count}}</span>
           <span class="label">视频数量</span>
         </li>
         <li class="item">
-          <span class="num">50</span>
+          <span class="num">{{dataSource.video_count}}</span>
           <span class="label">文章数量</span>
         </li>
         <li class="item">
-          <span class="num">50</span>
+          <span class="num">{{dataSource.comment_good_per}}</span>
           <span class="label">好评率</span>
         </li>
       </ul>
